e665d0e6413486056768936b01dbffb8a8f402d5
Leila Saryimova <saryymova@mail.ru>
2025-03-17 13:34:19 +0300

    delete test

--- auth_service/main_auth/tests.py
+++ auth_service/main_auth/tests.py
@@ -1,12 +1,1 @@
 import requests
-
-url = 'http://127.0.0.1:8000/api/register/'
-data = {
-    'username': 'testuser3',
-    'password': 'testpassword123',
-    'password2': 'testpassword123',
-    'token_api': '6b06d2f4d7c4e14e708e'
-}
-
-response = requests.post(url, json=data)
-print(response.json())
