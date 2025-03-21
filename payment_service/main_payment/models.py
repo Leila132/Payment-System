1088452e299a540c650348acc5044cf96dbcf9f3
Leila Saryimova <saryymova@mail.ru>
2025-03-21 16:01:51 +0300

    add status to payment model

--- payment_service/main_payment/models.py
+++ payment_service/main_payment/models.py
@@ -8,6 +8,9 @@ class Payment(models.Model):
         "Валюта", max_length=3, choices=[("NGN", "NGN"), ("GHS", "GHS")], default="NGN"
     )
     token = models.CharField("Токен платежа", max_length=100)
+    status = models.CharField(
+        "Статус", max_length=100, choices=[("Создан", "Создан"), ("Подтвержден", "Подтвержден"), ("Отклонен", "Отклонен"), ("Завершен", "Завершен")], default="Создан"
+    )
 
     def __str__(self):
         return self.name
